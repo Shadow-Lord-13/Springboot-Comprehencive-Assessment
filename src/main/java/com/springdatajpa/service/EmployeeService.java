47da8878730e18693dc35c90bbf45447ef3a67e3
Tushar Singh Bisht <tusharbisht2011@gmail.com>
2022-01-14 17:35:47 +0530

    Third Commit

--- src/main/java/com/springdatajpa/service/EmployeeService.java
+++ src/main/java/com/springdatajpa/service/EmployeeService.java
@@ -3,14 +3,37 @@ package com.springdatajpa.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.springdatajpa.entity.Employee;
+import com.springdatajpa.repository.EmployeeRepository;
 
-public interface EmployeeService {
+@Service
+public class EmployeeService{
 
-	public Employee saveEmployee(Employee employee);
-	public List<Employee> getEmployees();
-	public Employee getEmployeeById(int id);
-	public Employee updateEmployee(Integer salary, Integer id);
-	public Optional<Employee> checkEmployee(int id);
+	@Autowired
+	private EmployeeRepository repository;
+
+	public Employee saveEmployee(Employee employee) {
+		return repository.save(employee);
+	}
+
+	public List<Employee> getEmployees(){
+		return repository.findByOrderBySalaryDescNameAsc();
+	}
+
+	public Employee getEmployeeById(int id){
+		return repository.findById(id).orElse(null);
+	}
+
+	public Employee updateEmployee(Integer salary, Integer id) {
+		Employee existingEmployee = repository.findById(id).get();
+		existingEmployee.setSalary(salary);
+		return repository.save(existingEmployee);
+	}
+
+	public Optional<Employee> checkEmployee(int id){
+		return repository.findById(id);
+	}
 
 }
